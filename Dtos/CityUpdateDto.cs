240740af4eaa482170efe8b7d6dc838f6bced350
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2022-06-25 11:38:42 +0600

    added validations for citydto and cityname dto

--- Dtos/CityUpdateDto.cs
+++ Dtos/CityUpdateDto.cs
@@ -1,7 +1,12 @@
-﻿namespace BuyandRentHomeWebAPI.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuyandRentHomeWebAPI.Dtos
 {
     public class CityUpdateDto
     {
+        [Required(ErrorMessage = "Name is mandatory field")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "String length must be between 2-50")]
+        [RegularExpression(".*[a-zA-Z]+.*", ErrorMessage = "Only numerics are not allowed")]
         public string Name { get; set; }
     }
 }
--- Models/City.cs
+++ Models/City.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BuyandRentHomeWebAPI.Models
 {
     public class City
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Country { get; set; }
         public DateTime LastUpdated { get; set; }
         public int LastUpdateBy { get; set; }
