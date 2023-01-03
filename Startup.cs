390f6ad3f625706f2dd682272324c3620d7ddd33
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2023-01-03 23:00:23 +0600

    test commit

--- Startup.cs
+++ Startup.cs
@@ -29,7 +29,7 @@ namespace BuyandRentHomeWebAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container. 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<BuyRentHomeDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
