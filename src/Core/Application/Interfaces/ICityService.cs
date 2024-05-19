d40ecdf152028234c667798ca5e52e144dd1bfb2
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2024-05-19 17:50:43 +0600

    updated city service

--- src/Core/Application/Interfaces/ICityService.cs
+++ src/Core/Application/Interfaces/ICityService.cs
@@ -1,5 +1,8 @@
-﻿namespace Application.Interfaces;
+﻿using Application.DTOs;
+
+namespace Application.Interfaces;
 
 public interface ICityService
 {
+    Task<List<CityDto>> GetCityListByCountry(int countryId);
 }
--- src/Core/Application/Services/CityService.cs
+++ src/Core/Application/Services/CityService.cs
@@ -1,7 +1,20 @@
-﻿using Application.Interfaces;
+﻿using Application.DTOs;
+using Application.Interfaces;
+using AutoMapper;
+using Domain.Interfaces.Data;
 
 namespace Application.Services;
 
-public class CityService : ICityService
+public class CityService(IMapper mapper, IUnitOfWork unitOfWork) : ICityService
 {
+    private readonly IMapper mapper = mapper;
+    private readonly IUnitOfWork unitOfWork = unitOfWork;
+
+    public async Task<List<CityDto>> GetCityListByCountry(int countryId)
+    {
+        var cityList = await unitOfWork.CityRepository.GetCitiesByCountryAsync(countryId);
+        var cityDto = mapper.Map<List<CityDto>>(cityList);
+        return cityDto;
+
+    }
 }
